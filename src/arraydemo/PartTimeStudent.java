3ac3d98f0869ddd66b1b196558114f421a0afccd
Sidhak gill <gillsid@sheridancollege.ca>
2024-01-30 12:49:31 -0500

    edit

---
+++ src/arraydemo/PartTimeStudent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemo;
+
+/**
+ *
+ * @author gills
+ */
+
+
+public class PartTimeStudent extends Student{
+    private int numOfCourses;
+    
+    public PartTimeStudent(int numOfCourses, int sid, String sname) {
+        super(sid, sname);
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
