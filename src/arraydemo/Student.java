3ac1768a1cd447d1c71eb7776d435167d7a97a72
Sidhak gill <gillsid@sheridancollege.ca>
2024-01-30 11:54:52 -0500

    modified

---
+++ src/arraydemo/Student.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * this has been modified by sidhak on 30 jan at 11:53am
+ * @author gills
+ */
+public class Student {
+    private int sid ;
+    private String name;
+    private String Address;
+
+    public Student(int sid, String name) {
+        this.sid = sid;
+        this.name = name;
+    }
+    
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+  
+    
+}
+
